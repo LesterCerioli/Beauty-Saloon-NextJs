1a85eeb43f27b293c89daedae306357b7a1c08d1
artur-jansen <artur.jansenas@gmail.com>
2024-07-11 14:02:55 -0300

    feat(#1732): Build form stylezation

--- src/components/OnBoarding/OnBoarding.tsx
+++ src/components/OnBoarding/OnBoarding.tsx
@@ -1,5 +1,5 @@
-"use client";
 
+"use client";
 import React, { useState } from "react";
 import InputMask from "react-input-mask";
 import { BsArrowRightCircleFill } from "react-icons/bs";
@@ -9,53 +9,84 @@ function OnBoarding() {
     const [razaoSocial, setRazaoSocial] = useState('');
     const [cnpj, setCnpj] = useState('');
     const [tipoAtendimento, setTipoAtendimento] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
 
     return (
-        <form className="flex flex-col justify-center items-center bg-neutral-200 w-72 h-auto py-12 gap-3 rounded-xl">
-            <label className="w-64">Nome fantasia da empresa:</label>
-            <input 
-                type="text" 
-                placeholder="Insira aqui..." 
-                value={nomeFantasia} 
-                onChange={(e) => setNomeFantasia(e.target.value)}
-                className="w-64 px-2"
-            />
+        <form className="flex flex-col justify-center items-center bg-custom-principal w-72 h-auto py-8 gap-3 rounded-xl">
+            <div className="flex flex-col mb-2 gap-1 text-custom-secundaria">
+                <label className="w-64">Nome fantasia da empresa:</label>
+                <input 
+                    type="text" 
+                    placeholder="Insira o nome fantasia" 
+                    value={nomeFantasia} 
+                    onChange={(e) => setNomeFantasia(e.target.value)}
+                    className="w-60 px-2 py-1 rounded-xl transition-all duration-500 focus:w-64"
+                />
+            </div>
+            
+            <div className="flex flex-col mb-2 gap-1 text-custom-secundaria">
+                <label className="w-64">Razão social da empresa:</label>
+                <input 
+                    type="text" 
+                    placeholder="Insira a razão social" 
+                    value={razaoSocial} 
+                    onChange={(e) => setRazaoSocial(e.target.value)}
+                    className="w-60 px-2 py-1 rounded-xl transition-all duration-500 focus:w-64"
+                />
+            </div>
 
-            <label className="w-64">Razão social da empresa:</label>
-            <input 
-                type="text" 
-                placeholder="Insira aqui..." 
-                value={razaoSocial} 
-                onChange={(e) => setRazaoSocial(e.target.value)}
-                className="w-64 px-2"
-            />
+            <div className="flex flex-col mb-2 gap-1 text-custom-secundaria">
+                <label className="w-64">CNPJ da empresa:</label>
+                <InputMask 
+                    className="w-60 px-2 py-1 rounded-xl transition-all duration-500 focus:w-64"
+                    mask="99.999.999/9999-99" 
+                    placeholder="Insira o CNPJ"
+                    value={cnpj} 
+                    onChange={(e) => setCnpj(e.target.value)}
+                    required
+                >
+                </InputMask>
+            </div>
 
-            <label className="w-64">CNPJ da empresa:</label>
-            <InputMask 
-                className="w-64 px-2"
-                mask="99.999.999/9999-99" 
-                placeholder="Insira aqui"
-                value={cnpj} 
-                onChange={(e) => setCnpj(e.target.value)}
-                required
-            >
-            </InputMask>
+            <div className="flex flex-col mb-2 gap-1">
+                <label className="w-64 text-custom-secundaria">Especialização do salão:</label>
+                <select 
+                    value={tipoAtendimento} 
+                    onChange={(e) => setTipoAtendimento(e.target.value)} 
+                    className="w-60 py-1 rounded-xl transition-all duration-500 focus:w-64 text-gray-400"
+                >
+                    <option value="">Selecione...</option>
+                    <option className="w-full">Salão Masculino</option>
+                    <option className="w-full">Salão Feminino</option>
+                    <option className="w-full">Salão Unissex</option>
+                </select>
+            </div>
 
-            <label className="w-64">Selecione o tipo de atendimento:</label>
-            <select 
-                value={tipoAtendimento} 
-                onChange={(e) => setTipoAtendimento(e.target.value)} 
-                className="w-64"
-            >
-                <option value="">Selecione...</option>
-                <option className="w-full">Salão Masculino</option>
-                <option className="w-full">Salão Feminino</option>
-                <option className="w-full">Salão Unissex</option>
-            </select>
+            <div className="flex flex-col mb-2 gap-1 text-custom-secundaria">
+                <label className="w-64">Email:</label>
+                <input
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email"
+                    className="w-60 px-2 py-1 rounded-xl transition-all duration-500 focus:w-64" 
+                    placeholder="Insira o email"
+                />
+            </div>
 
+            <div className="flex flex-col mb-2 gap-1 text-custom-secundaria">
+                <label className="w-64">Senha:</label>
+                <input
+                    value={senha} 
+                    onChange={(e) => setSenha(e.target.value)}
+                    type="password"  // Mudei para 'password' para campo de senha
+                    className="w-60 px-2 py-1 rounded-xl transition-all duration-500 focus:w-64" 
+                    placeholder="Crie uma senha"
+                /> 
+            </div>
             <button 
                 type="submit" 
-                className="border border-black px-4 py-2 rounded flex items-center gap-2"
+                className="border border-transparent px-4 py-2 rounded-xl flex items-center gap-2 bg-custom-secundaria text-white"
             >
                 Cadastrar <BsArrowRightCircleFill />
             </button>
