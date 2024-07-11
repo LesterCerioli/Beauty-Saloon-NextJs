c3eada0539849508c842e65f828671d2753ff684
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-07-11 14:27:32 -0300

    feat(#1734): adding global styles and finishing initial banner from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
-import Image from "next/image";
-import { FaMagnifyingGlass } from "react-icons/fa6";
+import { Home } from "./pages/home";
 
-export default function Home() {
+export default function app() {
   return (
     <>
-      <main className="flex mt-6 p-4">
-        <div className="flex flex-1 flex-col">
-          <h1 className="text-[#111111] text-2xl font-bold">Olá, Placeholder</h1>
-          <span className="text-[#50555C] text-sm">Encontre o serviço que procura, e cuide de sí mesma!</span>
-        </div>
-        <button className="flex text-white items-center justify-center h-12 w-12 bg-btn-primary-color rounded-full">
-          <FaMagnifyingGlass size={24} />
-        </button>
-      </main>
+      <Home />
     </>
   );
 }
