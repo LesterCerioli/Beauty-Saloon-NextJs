2817f90836d75d9f694577b026a5b606431d71e2
Davi jansen <davi.jansenas@gmail.com>
2024-07-12 15:16:32 -0300

    feat(#1756): create interface for view Customer

---
+++ src/app/pages/ViewCustomer/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next"
+
+export const ViewCustomer: NextPage = () => {
+    return(
+        <main className="flex justify-center h-screen items-center bg-color-secundaria">
+        </main>
+    )     
+}
