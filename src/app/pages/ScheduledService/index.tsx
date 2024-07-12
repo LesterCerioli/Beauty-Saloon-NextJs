abf15f0f066e2d5ba5c1b253fe2595be15990ae6
artur-jansen <artur.jansenas@gmail.com>
2024-07-12 11:36:43 -0300

    feat(#1757): Atualization

---
+++ src/app/pages/ScheduledService/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+export const ScheduledService: NextPage = () =>{
+    return(
+        <main className="flex justify-center h-screen items-center bg-custom-secundaria">
+            
+        </main>
+    );
+}
---
+++ src/app/pages/WorkerRegistration/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+export const WorkerRegistration: NextPage = () =>{
+    return(
+        <main className="flex justify-center h-screen items-center bg-custom-secundaria">
+            
+        </main>
+    );
+}
