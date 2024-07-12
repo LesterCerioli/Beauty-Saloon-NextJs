f5a67df3f0cd9115be52e7be2587ec5957ab1025
artur-jansen <artur.jansenas@gmail.com>
2024-07-12 13:57:17 -0300

    feat(#1732): Atualization form

--- src/app/pages/OnBoarding/index.tsx
+++ src/app/pages/OnBoarding/index.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 import React, { useState } from "react";
-import InputMask from "react-input-mask";
+import InputMask from 'react-input-mask';
 import { BsArrowRightCircleFill } from "react-icons/bs";
 import { NextPage } from "next";
 
