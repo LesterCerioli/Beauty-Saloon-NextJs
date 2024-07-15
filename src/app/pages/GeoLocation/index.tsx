51a31b05cd1dc5ffd8bce015d160fb98d6a66ac8
artur-jansen <artur.jansenas@gmail.com>
2024-07-15 13:48:04 -0300

    feat(#1752): Build GeoLocation test

--- src/app/pages/GeoLocation/index.tsx
+++ src/app/pages/GeoLocation/index.tsx
@@ -1,20 +1,43 @@
 "use client";
 import { NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IoEnterOutline } from "react-icons/io5";
+import { fetchSaloes, Salao } from  "../../../apiteste"
+import { TiScissorsOutline } from "react-icons/ti";
+import { FaMapLocationDot } from "react-icons/fa6";
+import { SiGooglemaps } from "react-icons/si";
 
 export const GeoLocation: NextPage = () =>{
-    
+    const [saloes,  setSaloes] = useState<Salao[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await fetchSaloes();
+                setSaloes(data);
+            } catch (error) {
+                console.error('Erro ao buscar salões:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
-        <main className="flex justify-center h-screen items-center bg-color-secundaria">
-            <form className="flex flex-col justify-center items-center bg-color-principal w-72 h-auto py-8 gap-3 rounded-xl">
-                <button 
-                    type="submit" 
-                    className="border border-transparent px-4 py-2 rounded-xl flex items-center gap-2 bg-color-secundaria text-white"
-                >
-                    Acessar Salão <IoEnterOutline />
-                </button>
-            </form>
+        <main className="flex flex-col h-screen items-center bg-color-secundaria overflow-auto gap-5 py-8">
+            {saloes.map((salao, index) => (
+                <div key={index} className="flex flex-col justify-center bg-color-principal w-80 h-auto px-8 py-4 gap-2 rounded-xl">
+                    <h1 className="text-xl text-gray-500 flex items-center gap-2 border-b-color-secundaria border-b-2">{salao.nomeFantasia}<TiScissorsOutline /> </h1>
+                    <p className="flex items-center gap-1 text-lg"><FaMapLocationDot />{salao.localizacao.cidade} ({salao.localizacao.estado})</p>
+                    <p className="flex items-center gap-1"> <SiGooglemaps /> {salao.localizacao.rua} - Nº {salao.localizacao.numero}</p>
+                    <button 
+                        type="submit" 
+                        className="border border-transparent px-4 py-2 rounded-xl flex items-center gap-2 bg-color-secundaria text-white"
+                    >
+                        Acessar Salão <IoEnterOutline />
+                    </button>
+                </div>
+            ))}
         </main>
     );
 }
