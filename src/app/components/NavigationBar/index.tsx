98bf8c4a34928b47ca315bf2889638f75a15b2f0
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-07-23 12:00:44 -0300

    feat(#1769): build error fix

--- src/app/components/NavigationBar/index.tsx
+++ src/app/components/NavigationBar/index.tsx
@@ -14,7 +14,7 @@ export function NavigationBar(){
         // "fixed bottom-0 left-0 z-50 w-[768px]"
         <footer className="fixed bottom-0 left-0 z-50 inset-x-0 mx-auto max-w-[768px] min-w-min border-t-2 bg-white">
             <nav className="h-12 p-1 flex items-center justify-evenly text-zinc-400">
-                <Link className={pathname == "/" ? "flex items-center justify-evenly text-btn-primary-color" : "flex items-center justify-evenly text-zinc-400"} href="/">
+                <Link className={pathname == "/" || "/home" ? "flex items-center justify-evenly text-btn-primary-color" : "flex items-center justify-evenly text-zinc-400"} href="/home">
                     <MdHomeFilled size={24} />
                 </Link>
                 <Link className={pathname == "/nearby" ? "flex items-center justify-evenly text-btn-primary-color" : "flex items-center justify-evenly text-zinc-400"} href="#">
