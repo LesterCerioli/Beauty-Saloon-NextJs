36d1e2ae570057cb0685581b02ef2831b5c61bd6
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-07-23 11:55:24 -0300

    feat(#1769): changing home to act as index page

--- src/app/components/NavigationBar/index.tsx
+++ src/app/components/NavigationBar/index.tsx
@@ -14,19 +14,19 @@ export function NavigationBar(){
         // "fixed bottom-0 left-0 z-50 w-[768px]"
         <footer className="fixed bottom-0 left-0 z-50 inset-x-0 mx-auto max-w-[768px] min-w-min border-t-2 bg-white">
             <nav className="h-12 p-1 flex items-center justify-evenly text-zinc-400">
-                <Link className={pathname == "/home" ? "flex items-center justify-evenly text-btn-primary-color" : "flex items-center justify-evenly text-zinc-400"} href="/home">
+                <Link className={pathname == "/" ? "flex items-center justify-evenly text-btn-primary-color" : "flex items-center justify-evenly text-zinc-400"} href="/">
                     <MdHomeFilled size={24} />
                 </Link>
-                <Link className={pathname == "/nearby" ? "flex items-center justify-evenly text-btn-primary-color" : "flex items-center justify-evenly text-zinc-400"} href="/home">
+                <Link className={pathname == "/nearby" ? "flex items-center justify-evenly text-btn-primary-color" : "flex items-center justify-evenly text-zinc-400"} href="#">
                     <FaCompass  size={24} />
                 </Link>
-                <Link className={pathname == "/appointment" ? "flex items-center justify-evenly text-btn-primary-color" : "flex items-center justify-evenly text-zinc-400"} href="/home">
+                <Link className={pathname == "/appointment" ? "flex items-center justify-evenly text-btn-primary-color" : "flex items-center justify-evenly text-zinc-400"} href="#">
                     <FaCalendarAlt  size={24} />
                 </Link>
-                <Link className={pathname == "/inbox" ? "flex items-center justify-evenly text-btn-primary-color" : "flex items-center justify-evenly text-zinc-400"} href="/home">
+                <Link className={pathname == "/inbox" ? "flex items-center justify-evenly text-btn-primary-color" : "flex items-center justify-evenly text-zinc-400"} href="#">
                     <MdInbox  size={24} />
                 </Link>
-                <Link className={pathname == "/profile" ? "flex items-center justify-evenly text-btn-primary-color" : "flex items-center justify-evenly text-zinc-400"} href="/home">
+                <Link className={pathname == "/profile" ? "flex items-center justify-evenly text-btn-primary-color" : "flex items-center justify-evenly text-zinc-400"} href="#">
                     <IoPersonSharp  size={24} />
                 </Link>
             </nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Home from "./home/page";
+import Home from "./index/page";
 
 
 export default function app() {
   return (
     <>
-      {/* <Home /> */}
-      <h1>Index</h1>
+      <Home />
+      {/* <h1>Index</h1> */}
     </>
   );
 }
