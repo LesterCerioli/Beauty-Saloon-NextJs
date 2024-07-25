005a6ccb69726b06ae446367048eb79f4069e5b2
artur-jansen <artur.jansenas@gmail.com>
2024-07-25 14:32:53 -0300

    feat(#1752): Style modificated

--- src/app/GeoLocation/index.tsx
+++ src/app/GeoLocation/index.tsx
@@ -53,68 +53,68 @@ export const GeoLocation: NextPage = () => {
     };
 
     return (
-        <main className="flex flex-col h-screen items-center bg-color-secundaria overflow-auto gap-5 py-8 xl:flex-row xl:items-start">
-        <div className="flex flex-col gap-2 xl:gap-8 px-4 w-full max-w-xs xl:max-w-md">
-            <HeaderLocation
-            estado={estadoSelecionado}
-            cidade={cidadeSelecionada}
-            bairro={bairroSelecionado}
-            onEstadoChange={(estado) => {
-                setEstadoSelecionado(estado);
-                setCidadeSelecionada('');
-                setBairroSelecionado('');
-            }}
-            onCidadeChange={(cidade) => {
-                setCidadeSelecionada(cidade);
-                setBairroSelecionado('');
-            }}
-            onBairroChange={setBairroSelecionado}
-            />
-            
-            <button onClick={reloadPage} className="text-3xl bg-color-principal rounded-xl w-auto text-color-secundaria flex items-center">
-            <IoReloadCircle /> <p className="text-xl">Resetar busca</p>
-            </button>
-        </div>
-        
-        <div className="flex flex-col gap-6 xl:flex-row flex-wrap justify-center">
-            {filteredSaloes.length === 0 ? (
-            <div className="flex flex-col justify-center bg-color-principal h-auto w-full max-w-xs xl:max-w-sm px-4 py-4 gap-2 rounded-xl">
-                <h1 className="text-md text-red-500 flex items-center gap-1">
-                <MdErrorOutline /> Não existem salões cadastrados nessa localidade
-                </h1>
-            </div>
-            ) : (
-            filteredSaloes.map((salao, index) => (
-                <div key={index} className="flex flex-col justify-center bg-color-principal w-full max-w-xs xl:max-w-sm h-auto px-4 py-4 gap-2 rounded-xl">
-                <img
-                    src={salao.foto}
-                    alt={salao.nomeFantasia}
-                    className="w-full h-32 object-cover rounded-lg"
+        <main className="flex flex-row flex-wrap h-screen justify-center bg-color-secundaria overflow-auto gap-5 py-8 xl:items-start">
+            <div className="flex flex-col gap-2 px-4 w-full max-w-xs xl:max-w-md">
+                <HeaderLocation
+                    estado={estadoSelecionado}
+                    cidade={cidadeSelecionada}
+                    bairro={bairroSelecionado}
+                    onEstadoChange={(estado) => {
+                        setEstadoSelecionado(estado);
+                        setCidadeSelecionada('');
+                        setBairroSelecionado('');
+                    }}
+                    onCidadeChange={(cidade) => {
+                        setCidadeSelecionada(cidade);
+                        setBairroSelecionado('');
+                    }}
+                    onBairroChange={setBairroSelecionado}
                 />
-                <h1 className="text-xl text-gray-500 flex items-center gap-2 border-b-color-secundaria border-b-2">
-                    {salao.nomeFantasia} <TiScissorsOutline />
-                </h1>
-                <p className="flex items-center gap-1 text-lg">
-                    <FaMapLocationDot /> {salao.localizacao.cidade} ({salao.localizacao.estado})
-                </p>
-                <p className="flex flex-col gap-1">
-                    <div className="flex items-center gap-1">
-                    <SiGooglemaps /> {salao.localizacao.bairro}
-                    </div>
-                    <div className="flex items-center gap-1">
-                    <FaLocationCrosshairs /> {salao.localizacao.rua} - Nº {salao.localizacao.numero}
-                    </div>
-                </p>
-                <button
-                    type="submit"
-                    className="border border-transparent px-4 py-2 rounded-xl flex items-center gap-2 bg-color-secundaria text-white"
-                >
-                    Acessar Salão <IoEnterOutline />
+                
+                <button onClick={reloadPage} className="text-3xl bg-color-principal rounded-xl w-auto text-color-secundaria flex items-center">
+                    <IoReloadCircle /> <p className="text-xl">Resetar busca</p>
                 </button>
-                </div>
-            ))
-            )}
-        </div>
-    </main>
+            </div>
+            
+            <div className="flex flex-wrap justify-center gap-6 w-full">
+                {filteredSaloes.length === 0 ? (
+                    <div className="flex flex-col justify-center bg-color-principal h-auto w-full max-w-xs xl:max-w-sm px-4 py-4 gap-2 rounded-xl">
+                        <h1 className="text-md text-red-500 flex items-center gap-1">
+                            <MdErrorOutline /> Não existem salões cadastrados nessa localidade
+                        </h1>
+                    </div>
+                ) : (
+                    filteredSaloes.map((salao, index) => (
+                        <div key={index} className={`flex flex-col justify-center bg-color-principal w-full max-w-xs xl:max-w-sm h-auto px-4 py-4 gap-2 rounded-xl ${filteredSaloes.length === 1 ? 'h-screen' : ''}`}>
+                            <img
+                                src={salao.foto}
+                                alt={salao.nomeFantasia}
+                                className="w-full h-32 object-cover rounded-lg"
+                            />
+                            <h1 className="text-xl text-gray-500 flex items-center gap-2 border-b-color-secundaria border-b-2">
+                                {salao.nomeFantasia} <TiScissorsOutline />
+                            </h1>
+                            <p className="flex items-center gap-1 text-lg">
+                                <FaMapLocationDot /> {salao.localizacao.cidade} ({salao.localizacao.estado})
+                            </p>
+                            <p className="flex flex-col gap-1">
+                                <div className="flex items-center gap-1">
+                                    <SiGooglemaps /> {salao.localizacao.bairro}
+                                </div>
+                                <div className="flex items-center gap-1">
+                                    <FaLocationCrosshairs /> {salao.localizacao.rua} - Nº {salao.localizacao.numero}
+                                </div>
+                            </p>
+                            <button
+                                type="submit"
+                                className="border border-transparent px-4 py-2 rounded-xl flex items-center gap-2 bg-color-secundaria text-white"
+                            >
+                                Acessar Salão <IoEnterOutline />
+                            </button>
+                        </div>
+                    ))
+                )}
+            </div>
+        </main>
     );
 };
